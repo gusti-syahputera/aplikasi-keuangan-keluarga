37eaa65326522e5fbccb8e98032353f4935266e1
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2019-12-25 21:52:42 +0800

    Add test in case a user provides wrong password for authentication
    - Plus make use of the FamilyMemberException

--- src/test/java/access/control/AccessControlTest.java
+++ src/test/java/access/control/AccessControlTest.java
@@ -2,11 +2,11 @@ package access.control;
 
 import aplikasi.keuangan.keluarga.AplikasiKeuanganKeluarga;
 import aplikasi.keuangan.keluarga.FamilyMember;
+import aplikasi.keuangan.keluarga.FamilyMemberException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.SQLException;
 
 public class AccessControlTest {
 
@@ -19,15 +19,12 @@ public class AccessControlTest {
     @Test
     public void testUserLogin() {
         /* An unauthenticated user is trying to authenticate themselves as
-         * a family member.
-         */
+         * a family member. */
         int member_id = 1;
         FamilyMember member = null;
         try {
             member = new FamilyMember(member_id);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        } catch (FamilyMemberException ignored) {}
         String password = "T5B5YwyctYmhFS5J";  //derived key: "08CBC79A4722D5DB:1000:6F578C7316A2776E64E166B8283AC2F49D82BFFB"
 
         /* The user clicks "Login" button */
@@ -43,5 +40,22 @@ public class AccessControlTest {
         Assert.assertEquals(member, AplikasiKeuanganKeluarga.getAccessControl().getIdentity());
     }
 
-    /* TODO: Add test in case a user provides wrong password */
+    @Test
+    public void testUserLoginFail() {
+        /* An unauthenticated user is trying to authenticate themselves as
+         * a family member. */
+        int member_id = 1;
+        FamilyMember member = null;
+        try {
+            member = new FamilyMember(member_id);
+        } catch (FamilyMemberException ignored) {}
+        String wrong_password = "T5B5YwyctYmhFS5JWRONG";  //wrong pass
+
+        /* The user clicks "Login" button */
+        try {
+            AccessControl access_control = new AccessControl(member, wrong_password);
+            AplikasiKeuanganKeluarga.assignAccessControl(access_control);
+            Assert.fail("AuthFailException was not thrown.");
+        } catch (AuthFailException expected) {}
+    }
 }
