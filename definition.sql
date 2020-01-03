8ac807d2db574301b65b8324b16aaea7d89e30b5
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2020-01-03 13:19:19 +0000

    Remove unnecessary comments in definition.sql

--- definition.sql
+++ definition.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS member (
        member_id  INTEGER PRIMARY KEY,
        full_name  TEXT    NOT NULL,
-       birth_date TEXT,  -- ISO8601 string: "YYYY-MM-DD"
+       birth_date TEXT,
        role       INTEGER DEFAULT 0, -- Enum(ORDINARY, ACCOUNTANT, CHIEF)
        pass_key   TEXT    DEFAULT NULL
 );
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS account (
 CREATE TABLE IF NOT EXISTS transaction_ (
        tx_id       INTEGER PRIMARY KEY,
        account_id  INTEGER NOT NULL,
-       date_       TEXT    NOT NULL,  -- ISO8601 string: "YYYY-MM-DD"
+       date_       TEXT    NOT NULL,
        amount      NUMERIC NOT NULL,
        description TEXT,
        FOREIGN KEY (account_id) REFERENCES account(account_id)
