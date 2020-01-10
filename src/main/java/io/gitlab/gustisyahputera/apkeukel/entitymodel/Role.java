b113e989e9e4c6838e6df39ef14625f7c072a001
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2020-01-10 21:57:17 +0800

    Assign privileges to Role instances

--- src/main/java/io/gitlab/gustisyahputera/apkeukel/entitymodel/Role.java
+++ src/main/java/io/gitlab/gustisyahputera/apkeukel/entitymodel/Role.java
@@ -1,4 +1,21 @@
 package io.gitlab.gustisyahputera.apkeukel.entitymodel;
 
 
-public enum Role {ORDINARY, ACCOUNTANT, CHIEF}
+public enum Role {
+
+    ORDINARY(false, false, false),
+    ACCOUNTANT(false, true, true),
+    CHIEF(true, false, false);
+
+    public boolean canManageFamilyMember;
+    public boolean canManageAccount;
+    public boolean canManageTransaction;
+
+    Role(boolean canManageFamilyMember,
+         boolean canManageAccount,
+         boolean canManageTransaction) {
+        this.canManageFamilyMember = canManageFamilyMember;
+        this.canManageAccount = canManageAccount;
+        this.canManageTransaction = canManageTransaction;
+    }
+}
